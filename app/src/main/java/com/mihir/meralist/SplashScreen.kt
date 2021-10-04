dab40229fa594c379226fd4b33822b2a6523f7b7
Parikshit Agarwal <parikshitagarwal134@gmail.com>
2021-10-05 01:45:05 +0530

    card view and ui changes

--- app/src/main/java/com/mihir/meralist/SplashScreen.kt
+++ app/src/main/java/com/mihir/meralist/SplashScreen.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.widget.Toast
+
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
