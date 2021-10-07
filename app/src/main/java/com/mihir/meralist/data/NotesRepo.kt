54899116c5146dcb9bdc4c9752178df13c20661a
siddhraj <skrathod282001@gmail.com>
2021-10-08 00:04:56 +0530

    declared database accessing function (Dao & Repo) as suspend function

--- app/src/main/java/com/mihir/meralist/data/NotesRepo.kt
+++ app/src/main/java/com/mihir/meralist/data/NotesRepo.kt
@@ -7,17 +7,17 @@ class NotesRepo(private val dao:Notes_Dao) {
     val readAllData: LiveData<List<Notes>> = dao.readAllNodes()
     //lateinit var selectedNote: Notes
 
-    fun addNote(note: Notes){
+    suspend fun addNote(note: Notes){
 
         dao.addNote(note)
     }
-    fun updateNote(note: Notes){
+    suspend fun updateNote(note: Notes){
         dao.updateNote(note)
     }
     fun sendNote(sentId:Int){
         //selectedNote = dao.sendNote(sentId)
     }
-    fun deleteNote(note: Notes){
+   suspend fun deleteNote(note: Notes){
         dao.deleteNote(note)
     }
 }
